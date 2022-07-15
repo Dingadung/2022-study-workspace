46d10683fd6d2108e8b7624e4a42606dd1b464d9
Dingadung <1229jimin@naver.com>
2022-07-15 14:24:57 +0900

    2022.07.15 ADD Board.class -ing

--- board-app/app/src/main/java/com/bitcamp/board/BoardHandler.java
+++ board-app/app/src/main/java/com/bitcamp/board/BoardHandler.java
@@ -7,20 +7,14 @@ public class BoardHandler {
   //-------------------변수 선언-----------------------\\
   static int boardCount = 0; // 저장된 게시글의 개수
   static final int SIZE = 3;
-  static int[] no = new int[SIZE];
-  static String[] title = new String[SIZE];
-  static String[] content = new String[SIZE];
-  static String[] writer = new String[SIZE];
-  static String[] password = new String[SIZE];
-  static int[] viewCount = new int[SIZE];
-  static long[] createdDate = new long[SIZE];
+  static Board[] boards = new Board[SIZE];
   //-------------------변수 선언-----------------------\\
 
   static void processList() {
     //------------------- menu 1 --------------------------
     java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat(
-      "yyyy-MM-dd hh:mm:ss"
-    );
+        "yyyy-MM-dd hh:mm:ss"
+        );
 
     System.out.println("[게시글 목록]");
 
@@ -29,13 +23,13 @@ public class BoardHandler {
       java.util.Date date = new java.util.Date(createdDate[i]);
       String dateStr = formatter.format(date);
       System.out.printf(
-        "%d\t%s\t\t%d\t%s\t%s\n",
-        no[i],
-        title[i],
-        viewCount[i],
-        writer[i],
-        dateStr
-      );
+          "%d\t%s\t\t%d\t%s\t%s\n",
+          no[i],
+          title[i],
+          viewCount[i],
+          writer[i],
+          dateStr
+          );
     }
   }
 
@@ -74,21 +68,28 @@ public class BoardHandler {
 
     if (SIZE == boardCount) {
       System.out.println(
-        "더 이상 게시글을 등록할 수 없습니다. 최대 게시물 등록 수를 초과했습니다!"
-      );
+          "더 이상 게시글을 등록할 수 없습니다. 최대 게시물 등록 수를 초과했습니다!"
+          );
       return;
     }
 
-    title[boardCount] = Prompt.inputString("제목? ");
-    content[boardCount] = Prompt.inputString("내용? ");
-    writer[boardCount] = Prompt.inputString("작성자? ");
-    password[boardCount] = Prompt.inputString("암호? ");
+    boards[boardCount] = new Board();
+    //    title[boardCount] = Prompt.inputString("제목? ");
+    //    content[boardCount] = Prompt.inputString("내용? ");
+    //    writer[boardCount] = Prompt.inputString("작성자? ");
+    //    password[boardCount] = Prompt.inputString("암호? ");
+    boards[boardCount].title = Prompt.inputString("제목? ");
+    boards[boardCount].content = Prompt.inputString("내용? ");
+    boards[boardCount].writer= Prompt.inputString("작성자? ");
+    boards[boardCount].password = Prompt.inputString("암호? ");
 
-    no[boardCount] = boardCount == 0 ? 1 : no[boardCount - 1] + 1;
-    
-    createdDate[boardCount] = System.currentTimeMillis();
+    //no[boardCount] = boardCount == 0 ? 1 : no[boardCount - 1] + 1;
 
-    viewCount[boardCount] = 0;
+    boards[boardCount].createdDate = System.currentTimeMillis();
+
+    boards[boardCount].viewCount = 0;
+
+    // 새로만든 인스턴스 주소를 레퍼런스 배열에 저장한다.
 
     boardCount++;
   }
