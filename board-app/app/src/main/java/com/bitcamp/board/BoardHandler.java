748128222426833547483761ab4035863c14747b
Dingadung <1229jimin@naver.com>
2022-07-15 15:15:48 +0900

    2022.07.15 EDIT Board.class final

--- board-app/app/src/main/java/com/bitcamp/board/BoardHandler.java
+++ board-app/app/src/main/java/com/bitcamp/board/BoardHandler.java
@@ -20,14 +20,15 @@ public class BoardHandler {
 
     System.out.println("번호\t제목\t\t조회수\t작성자\t등록일");
     for (int i = 0; i < boardCount; i++) {
-      java.util.Date date = new java.util.Date(createdDate[i]);
+      Board board = boards[i];
+      java.util.Date date = new java.util.Date(board.createdDate);
       String dateStr = formatter.format(date);
       System.out.printf(
           "%d\t%s\t\t%d\t%s\t%s\n",
-          no[i],
-          title[i],
-          viewCount[i],
-          writer[i],
+          board.no,
+          board.title,
+          board.viewCount,
+          board.writer,
           dateStr
           );
     }
@@ -35,30 +36,30 @@ public class BoardHandler {
 
   static void processDetail() {
     //------------------- menu 2 --------------------------
+    Board board = null;
     System.out.println("[게시판 상세보기]");
     int noBoard = Prompt.inputInt("몇 번 게시물을 조회하시겠습까? ");
-    int idx = -1;
     for (int i = 0; i < boardCount; i++) {
-      if (no[i] == noBoard) {
-        idx = i;
+      if (boards[i].no == noBoard) {
+        board = boards[i];
         break;
       }
     }
 
-    if (idx == -1) {
+    if (board == null) {
       System.out.println("해당 번호의 게시물은 존재하지 않습니다!");
       return; // 메소드를 호출할 곳으로 돌아가고 싶을 때. 
     }
 
-    viewCount[idx]++;
+    board.viewCount++;
 
-    System.out.printf("번호: %d\n", no[idx]);
-    System.out.printf("제목: %s\n", title[idx]);
-    System.out.printf("내용: %s\n", content[idx]);
-    System.out.printf("조회수: %d\n", viewCount[idx]);
-    System.out.printf("작성자: %s\n", writer[idx]);
+    System.out.printf("번호: %d\n", board.no);
+    System.out.printf("제목: %s\n", board.title);
+    System.out.printf("내용: %s\n", board.content);
+    System.out.printf("조회수: %d\n", board.viewCount);
+    System.out.printf("작성자: %s\n", board.writer);
 
-    java.util.Date date = new java.util.Date(createdDate[idx]);
+    java.util.Date date = new java.util.Date(board.createdDate);
     System.out.printf("등록일: %1$tY-%1$tm-%1$td/%1$tA %1$tH:%1$tM\n", date);
   }
 
@@ -73,17 +74,18 @@ public class BoardHandler {
       return;
     }
 
-    boards[boardCount] = new Board();
+    Board board = new Board();
+    boards[boardCount] = board;
     //    title[boardCount] = Prompt.inputString("제목? ");
     //    content[boardCount] = Prompt.inputString("내용? ");
     //    writer[boardCount] = Prompt.inputString("작성자? ");
     //    password[boardCount] = Prompt.inputString("암호? ");
-    boards[boardCount].title = Prompt.inputString("제목? ");
-    boards[boardCount].content = Prompt.inputString("내용? ");
-    boards[boardCount].writer= Prompt.inputString("작성자? ");
-    boards[boardCount].password = Prompt.inputString("암호? ");
+    board.title = Prompt.inputString("제목? ");
+    board.content = Prompt.inputString("내용? ");
+    board.writer= Prompt.inputString("작성자? ");
+    board.password = Prompt.inputString("암호? ");
 
-    //no[boardCount] = boardCount == 0 ? 1 : no[boardCount - 1] + 1;
+    board.no = boardCount == 0 ? 1 : boards[boardCount - 1].no + 1;
 
     boards[boardCount].createdDate = System.currentTimeMillis();
 
