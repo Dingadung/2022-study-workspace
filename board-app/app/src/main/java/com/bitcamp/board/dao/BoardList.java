96005526d93f0135f3a6189bf2b71a12857ad0a1
Dingadung <1229jimin@naver.com>
2022-07-29 20:21:03 +0900

    2022.07.29 java-study-note

--- board-app/app/src/main/java/com/bitcamp/board/dao/BoardList.java
+++ board-app/app/src/main/java/com/bitcamp/board/dao/BoardList.java
@@ -1,6 +1,7 @@
 package com.bitcamp.board.dao;
 
 import com.bitcamp.board.domain.Board;
+import com.bitcamp.util.ListException;
 import com.bitcamp.util.ObjectList;
 
 // 게시글 목록을 관리하는 역할
@@ -19,8 +20,9 @@ public class BoardList  extends ObjectList{
   //ObjectList의 get()에서 던지는 예외를 이 메서드에서 처리하지 않고 호출자에게 처리를 위임한다.
   // ListException은 Runtime 계열이기 때문에 메서드 선언부에 표시하지 않아도 된다.
   // Exception 계열의 예외를 다루는 것보다 덜 번거롭다.
+  // Throwable이 상위 클래스여도수퍼가 에러 안던지니까 얘도 던지면 안된다!
   @Override
-  public Board get(int boardNo){
+  public Board get(int boardNo)throws ListException{
     for (int i = 0; i < size(); i++) {
       Board board = (Board)super.get(i);
       if (board.no== boardNo) {
