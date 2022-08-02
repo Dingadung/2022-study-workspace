d899d1aa82ea48087257c0f7e09f76d391a6354e
Dingadung <1229jimin@naver.com>
2022-08-02 10:08:20 +0900

    board-app 21버전 초기, LinkedList 소스 파일 추가

--- board-app/app/src/main/java/com/bitcamp/study/board/domain/Member.java
+++
@@ -1,9 +0,0 @@
-package com.bitcamp.study.board.domain;
-
-public class Member {
-  public int no;
-  public String name;
-  public String email;
-  public String password;
-  public long createdDate;
-}
--- board-app/app/src/main/java/com/bitcamp/util/Node.java
+++ board-app/app/src/main/java/com/bitcamp/util/Node.java
@@ -1,13 +1,23 @@
 package com.bitcamp.util;
 
+/**
+ * 연결 리스트의 각 항목의 값을 저장하는 일을 할 클래스다.
+ * @author bitcamp
+ *
+ */
 public class Node {
+  Object value;
   Node prev;
   Node next;
-  Object value;
 
-  Node(){}
-  Node(Object value){
-    this.value = value;
+  public Node() {}
+
+  public Node(Object v) {
+    this.value = v;
   }
-
 }
+
+
+
+
+
