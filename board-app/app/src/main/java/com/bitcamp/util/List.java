729bedc3797ea37c9cb07e010b20e849304ac23a
Dingadung <1229jimin@naver.com>
2022-08-02 11:47:17 +0900

    board-app Add List Interface

--- board-app/app/src/main/java/com/bitcamp/util/List.java
+++ board-app/app/src/main/java/com/bitcamp/util/List.java
@@ -6,6 +6,12 @@ package com.bitcamp.util;
  *
  */
 public interface List {
-  void add(Object value); // 목록에 값을 더하는 메서드의 형식. 추상 메서드 형태. 적어주든, 안 적어주든, 무조건 public.
-
+  // 메서드 형식
+  // - 추상 메서드 형태
+  // - 무조건 public 으로 공개
+  void add(Object value); // 목록에 값을 더하는 메서드의 형식. 추상 메서드 형식. 적어주든, 안 적어주든, 무조건 public. 다른 접근 지정자는 불가합니당~~~ 컴파일러가 알아서 붙입니당~~
+  Object get(int index); // 목록에서 인덱스에 해당하는 항목을 꺼내는 메서드의 형식.
+  Object remove(int index); // 목록에서 인덱스에 해당하는 항목을 삭제하는 메서드의 형식.
+  Object[] toArray(); // 목록에 저장된 항목들을 배열에 담아 리턴하는 메서드의 형식.
+  int size(); // 목록에 저장된 항목의 개수를 리턴하는 메서드의 형식.
 }
