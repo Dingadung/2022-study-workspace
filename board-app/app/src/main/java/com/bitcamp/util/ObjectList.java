b169668f23115d42c943b3706554392243bed72c
Dingadung <1229jimin@naver.com>
2022-08-03 11:05:49 +0900

    Delete add feild, method in ObjectList

--- board-app/app/src/main/java/com/bitcamp/util/ObjectList.java
+++ board-app/app/src/main/java/com/bitcamp/util/ObjectList.java
@@ -2,11 +2,10 @@ package com.bitcamp.util;
 
 //List  규격에 따라 메서드를 구현할 것이라고 선언한다!
 // 만약 규격에 따라 메서드를 구현하지 않으면 컴파일을 안해준다!
-public class ObjectList implements List {
+public class ObjectList extends AbstractList{
 
   private static final int DEFAULT_CAPACITY = 10;
 
-  private int size;
   private Object[] elementData;
 
   public ObjectList() {
@@ -58,11 +57,6 @@ public class ObjectList implements List {
     elementData[--size] = null;
 
     return deleted;
-  }
-
-  @Override
-  public int size() {
-    return size;
   }
 
   private void grow() {
