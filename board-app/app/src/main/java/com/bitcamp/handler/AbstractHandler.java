bc0c4484ea8817af2c732a682e34de9e45345e3e
Dingadung <1229jimin@naver.com>
2022-08-04 10:16:38 +0900

    Add board-app AbstractHandler class

---
+++ board-app/app/src/main/java/com/bitcamp/handler/AbstractHandler.java
@@ -0,0 +1,6 @@
+package com.bitcamp.handler;
+
+// Handler 규격에 맞추어 핸들러의 서브 클래스에게 물려줄 공통 필드가 메서드를 구현한다.
+public abstract class AbstractHandler implements Handler {
+
+}
---
+++ board-app/app/src/main/java/stackTest/board/domain/Member.java
@@ -0,0 +1,9 @@
+package stackTest.board.domain;
+
+public class Member {
+  public int no;
+  public String name;
+  public String email;
+  public String password;
+  public long createdDate;
+}
