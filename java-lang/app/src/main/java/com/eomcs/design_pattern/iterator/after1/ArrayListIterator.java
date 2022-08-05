9924f4ee7c757444bcac9855a8b0d9510468c843
Dingadung <1229jimin@naver.com>
2022-08-05 12:11:47 +0900

    Add Iterator Interface and implementer

---
+++ java-lang/app/src/main/java/com/eomcs/design_pattern/iterator/after1/ArrayListIterator.java
@@ -0,0 +1,15 @@
+package com.eomcs.design_pattern.iterator.after1;
+
+// ArrayList에서 데이터를 꺼내줄 객체//
+public class ArrayListIterator implements Iterator {
+  @Override
+  public boolean hasNext() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+  @Override
+  public Object next() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
