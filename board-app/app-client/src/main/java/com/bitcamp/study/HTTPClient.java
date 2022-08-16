34c705549dcb3ecd038b31d0a6757cad6d075623
Dingadung <1229jimin@naver.com>
2022-08-16 12:29:58 +0900

    Add HTTPClient class

--- board-app/app-client/src/main/java/com/bitcamp/study/HTTPClient.java
+++ board-app/app-client/src/main/java/com/bitcamp/study/HTTPClient.java
@@ -1,5 +1,18 @@
 package com.bitcamp.study;
 
-public class HTTPClient {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
 
+public class HTTPClient {
+  public static void main(String[] args) throws Exception {
+    try(
+        Socket socket = new Socket("www.etnews.co.kr", 80);
+        BufferedReader in2 = new BufferedReader( new InputStreamReader(socket.getInputStream()));
+        PrintStream out2 = new PrintStream(socket.getOutputStream());){
+
+
+    }
+  }
 }
