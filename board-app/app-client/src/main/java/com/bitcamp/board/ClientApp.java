08a1882478220d0184a538e216350e778d1b2128
Dingadung <1229jimin@naver.com>
2022-08-16 11:21:37 +0900

    Add Client App socket

--- board-app/app-client/src/main/java/com/bitcamp/board/ClientApp.java
+++ board-app/app-client/src/main/java/com/bitcamp/board/ClientApp.java
@@ -7,8 +7,13 @@ public class ClientApp {
   public static void main(String[] args) {
     System.out.println("[게시글 관리 클라이언트]");
 
-    // 네트워킹 준비
-    Socket socket = new Socket();
-  }
+    // 네트워크 준비
+    try {
+      Socket socket = new Socket("127.0.0.1", 8888 ); // 같은 컴퓨터 내에 서버와 클라이언트가 존재하므로 정확한 IP address 대신, 127.0.0.1(local host)이 가능하다. (접속할 IP address, port num)
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    System.out.println("종료");
+  }//main()
 
 }
