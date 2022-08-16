13ef08325f99a276832d3222569683c5fce75a6d
Dingadung <1229jimin@naver.com>
2022-08-16 11:25:46 +0900

    Add board-app-server, ServerApp class

--- board-app/app-client/src/main/java/com/bitcamp/board/ClientApp.java
+++ board-app/app-client/src/main/java/com/bitcamp/board/ClientApp.java
@@ -8,11 +8,20 @@ public class ClientApp {
     System.out.println("[게시글 관리 클라이언트]");
 
     // 네트워크 준비
+    //=> 정상적으로 연결되었으면, Socket 객체를 리턴한다.
     try {
       Socket socket = new Socket("127.0.0.1", 8888 ); // 같은 컴퓨터 내에 서버와 클라이언트가 존재하므로 정확한 IP address 대신, 127.0.0.1(local host)이 가능하다. (접속할 IP address, port num)
+
+      System.out.println("연결되었음!");
+
+      // 네트워크 끊기
+      // => 서버와 연결된 것을 끊는다.
+      socket.close();
+      System.out.println("연결을 끊었음!");
     } catch (Exception e) {
       e.printStackTrace();
-    }
+    }//try-catch()
+
     System.out.println("종료");
   }//main()
 
