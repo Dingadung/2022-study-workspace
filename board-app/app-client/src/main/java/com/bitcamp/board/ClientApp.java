4174689efd5f198c15df7ebf14634ffbcd8be456
Dingadung <1229jimin@naver.com>
2022-08-16 15:47:42 +0900

    Edit ClientApp Handler to 명령형으로

--- board-app/app-client/src/main/java/com/bitcamp/board/ClientApp.java
+++ board-app/app-client/src/main/java/com/bitcamp/board/ClientApp.java
@@ -7,6 +7,7 @@ import java.util.Stack;
 import com.bitcamp.board.handler.BoardHandler;
 import com.bitcamp.board.handler.MemberHandler;
 import com.bitcamp.handler.Handler;
+import com.bitcamp.util.Prompt;
 
 public class ClientApp {
 
@@ -29,16 +30,57 @@ public class ClientApp {
 
       // 핸들러를 담을 레퍼런스 배열을 준비한다.
       Handler[] handlers = new Handler[] { // 파일명을 목적에 맞게 각각 전달
-          new BoardHandler("board.json"), // 게시판
-          new BoardHandler("reading.json"), // 독서록
-          new BoardHandler("visit.json"), // 방명록
-          new BoardHandler("notice.json"), // 공지사항
-          new BoardHandler("daily.json"), // 일기장
-          new MemberHandler("member.json") // 회원
+          new BoardHandler("board", in, out), // 게시판
+          new BoardHandler("reading", in, out), // 독서록
+          new BoardHandler("visit", in, out), // 방명록
+          new BoardHandler("notice", in, out), // 공지사항
+          new BoardHandler("daily", in, out), // 일기장
+          new MemberHandler("member", in, out) // 회원
       };
 
       // "메인" 메뉴의 이름을 스택에 등록한다.
       breadcrumbMenu.push("메인");
+
+      // 메뉴명을 저장할 배열을 준비한다.
+      String[] menus = {"게시판", "독서록", "방명록", "공지사항", "일기장", "회원"};
+
+      loop: 
+        while (true) {
+
+          // 메인 메뉴 출력
+          printTitle();
+
+          printMenus(menus);
+
+          System.out.println();
+
+          try {
+            int mainMenuNo = Prompt.inputInt("메뉴를 선택하세요[1..6](0: 종료) ");
+
+            if (mainMenuNo < 0 || mainMenuNo > menus.length) {
+              System.out.println("메뉴 번호가 옳지 않습니다!");
+              continue; // while 문의 조건 검사로 보낸다.
+
+            } else if (mainMenuNo == 0) {
+              break loop;
+            }
+
+            // 메뉴에 진입할 때 breadcrumb 메뉴바에 그 메뉴를 등록한다.
+            breadcrumbMenu.push(menus[mainMenuNo - 1]);
+
+            // 메뉴 번호로 Handler 레퍼런스에 들어있는 객체를 찾아 실행한다.
+            handlers[mainMenuNo - 1].execute();
+
+            breadcrumbMenu.pop();
+
+          } catch (Exception ex) {
+            System.out.println("입력 값이 옳지 않습니다.");
+          }
+
+
+        } // while
+
+      Prompt.close();
 
       // 통신 프로토콜에 따라 요청한다. 먼저 서버에 전송!
       out.writeUTF("board");
