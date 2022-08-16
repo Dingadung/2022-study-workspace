cad9e9fb9bbef0adc7bab6ec3129af34d1a1ab9c
Dingadung <1229jimin@naver.com>
2022-08-16 12:50:06 +0900

    Edit HTTPClientclass using stores

--- board-app/app-client/src/main/java/com/bitcamp/study/HTTPClient.java
+++ board-app/app-client/src/main/java/com/bitcamp/study/HTTPClient.java
@@ -8,13 +8,13 @@ import java.net.Socket;
 public class HTTPClient {
   public static void main(String[] args) throws Exception {
     try(
-        Socket socket = new Socket("www.auction.co.kr", 80); // web server port number: 80
+        Socket socket = new Socket("stores.auction.co.kr", 80); // web server port number: 80
         BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
         PrintStream out = new PrintStream(socket.getOutputStream());){
 
       // HTTP 프로토콜에 따라서 메인 웹 페이지를 요청한다.
-      out.println("GET / HTTP/1.1"); // root 에 있는 메인 페이지를원한다.
-      out.println("Host: www.auction.co.kr");
+      out.println("GET /thisbike HTTP/1.1"); // root 에 있는 메인 페이지를원한다.
+      out.println("Host: stores.auction.co.kr");
       out.println(); // 요청에 대한 정보 끝
 
       // 웹서버의 응답을 출력한다.
