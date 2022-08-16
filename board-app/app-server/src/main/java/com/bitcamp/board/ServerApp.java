dbc39ecd13cd15b4d6b22b2e83118008c04d5ff6
Dingadung <1229jimin@naver.com>
2022-08-16 11:28:28 +0900

    Add board-app-server, ServerApp class

--- board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
@@ -1,10 +1,21 @@
 package com.bitcamp.board;
 
+import java.net.ServerSocket;
+
 public class ServerApp {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.println("[게시글 데이터 관리 서버]");
-  }
+
+    try {
+      // 네트워크 준비
+      ServerSocket serverSocket = new ServerSocket(8888);
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }//try-catch()
+    System.out.println("서버 종료!");
+  }//main()
 
 }
