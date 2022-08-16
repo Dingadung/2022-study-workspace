2a55e0f15c008132e43620ccc9154595b59a6ff6
Dingadung <1229jimin@naver.com>
2022-08-16 12:59:50 +0900

    Add HTTPServer class

--- board-app/app-client/src/main/java/com/bitcamp/study/HTTPClient.java
+++ board-app/app-client/src/main/java/com/bitcamp/study/HTTPClient.java
@@ -9,8 +9,8 @@ public class HTTPClient {
   public static void main(String[] args) throws Exception {
     try(
         Socket socket = new Socket("stores.auction.co.kr", 80); // web server port number: 80
-        BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
-        PrintStream out = new PrintStream(socket.getOutputStream());){
+        BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream())); // (new InputStream)
+        PrintStream out = new PrintStream(socket.getOutputStream());){ // (OutputStream) // printStream이 무슨 역할이지?
 
       // HTTP 프로토콜에 따라서 메인 웹 페이지를 요청한다.
       out.println("GET /thisbike HTTP/1.1"); // root 에 있는 메인 페이지를원한다.
---
+++ board-app/app-server/src/main/java/com/bitcamp/study/HTTPServer.java
@@ -0,0 +1,10 @@
+package com.bitcamp.study;
+
+public class HTTPServer {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
