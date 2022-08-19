8e520bbf1deb54699559f8dab19ab2fb3ffb5902
Dingadung <1229jimin@naver.com>
2022-08-19 11:21:21 +0900

    Edit BoardDaoProxy class 생성자 수정

--- board-app/app-server/src/main/java/com/bitcamp/board/dao/BoardDaoProxy.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/dao/BoardDaoProxy.java
@@ -1,56 +1,25 @@
 package com.bitcamp.board.dao;
 
-import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.Iterator;
 import com.bitcamp.board.domain.Board;
-import com.google.gson.Gson;
 
 // BoardDao와 통신을 담당할 대행 객체 <- server 개발자 입장에서 개발 클라이언트 개발자를 위하여!
 
 //
 public class BoardDaoProxy {
+  String dataName;
   DataInputStream in;
   DataOutputStream out;
 
-  public BoardDaoProxy(DataInputStream in, DataOutputStream out) {
+  public BoardDaoProxy(String dataName, DataInputStream in, DataOutputStream out) {
+    this.dataName = dataName;
     this.in = in;
     this.out = out;
   }
 
-  public void load() throws Exception {
-    try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
 
-      // 파일에서 JSON 문자열을 모두 읽어 StringBuilder에  담는다.
-      StringBuilder stringBuilder = new StringBuilder();
-      String str;
-      while ((str = in.readLine()) != null) {
-        stringBuilder.append(str);
-      }
-
-      // StringBuilde에  보관된 JSON 문자열을 가지고 Board[]을 생성한다./
-      Board[] arr = new Gson().fromJson(stringBuilder.toString(), Board[].class);
-
-      // Board[]배열의 저장된 객체를 List로 옮긴다.
-      for(int i=0;i<arr.length;i++) {
-        list.add(arr[i]);
-      }
-
-      // 게시글 데이터를 로딩한 후, 마지막 게시글 번호를 설정해 준다.
-      // 새 게시물의 번호 설정
-      boardNo = arr[arr.length-1].no; 
-    }
-  }
-
-  public void save() throws Exception {
-    try (FileWriter out = new FileWriter(filename)) {
-      Board[] boards = list.toArray(new Board[0]); // 현재 list에 들어 있는 보드 객체들 받아오기
-      out.write( new Gson().toJson(boards));
-    }
-  }
 
   public void insert(Board board) {
     board.no = nextNo();
