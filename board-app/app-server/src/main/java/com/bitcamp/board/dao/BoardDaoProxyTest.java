0bd6073943e61ed60a1be53535af24008b75a25b
Dingadung <1229jimin@naver.com>
2022-08-19 11:39:33 +0900

    Add BOardDaoProxyTest class test findAll()

--- board-app/app-server/src/main/java/com/bitcamp/board/dao/BoardDaoProxyTest.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/dao/BoardDaoProxyTest.java
@@ -1,11 +1,25 @@
 package com.bitcamp.board.dao;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
+import com.bitcamp.board.domain.Board;
 
 public class BoardDaoProxyTest {
 
   public static void main(String[] args) throws Exception {
-    try(Socket socket = new Socket("127.0.0.1", 8888)){
+    try(Socket socket = new Socket("127.0.0.1", 8888);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())
+        ){
+      BoardDaoProxy boardDao = new BoardDaoProxy("board", in, out);
+
+      // Test1 목록 가져오기
+      Board[] boards =  boardDao.findAll();
+      for(Board b : boards) {
+        System.out.println(b);
+      }
+      System.out.println("------------------------------------------------------");
 
     }
   }
