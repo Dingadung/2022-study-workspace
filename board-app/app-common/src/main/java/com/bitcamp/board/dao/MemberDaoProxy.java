3f44a213b9e4bce7444a376e81641b58cf62f55a
Dingadung <1229jimin@naver.com>
2022-08-19 16:07:53 +0900

    Edit MemberDaoProxy Stateless

--- board-app/app-common/src/main/java/com/bitcamp/board/dao/MemberDaoProxy.java
+++ board-app/app-common/src/main/java/com/bitcamp/board/dao/MemberDaoProxy.java
@@ -2,6 +2,7 @@ package com.bitcamp.board.dao;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.net.Socket;
 import com.bitcamp.board.domain.Member;
 import com.google.gson.Gson;
 
@@ -10,56 +11,80 @@ import com.google.gson.Gson;
 
 public class MemberDaoProxy {
   String dataName;
-  DataInputStream in;
-  DataOutputStream out;
-
-  public MemberDaoProxy(String dataName, DataInputStream in, DataOutputStream out) {
+  String ip;
+  int port;
+  public MemberDaoProxy(String dataName, String ip, int port) {
     this.dataName = dataName;
-    this.in = in;
-    this.out = out;
+    this.ip = ip;
+    this.port = port;
   }
 
   public boolean insert(Member email)throws Exception {
-    out.writeUTF(dataName);
-    out.writeUTF("insert");
-    out.writeUTF(new Gson().toJson(email)); // json을 서버로 보내기
-    // 서버로부터 요청했던 데이터 읽어오기
-    return in.readUTF().equals("success");
+    try(Socket socket =  new Socket(ip, port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())
+        ){
+      out.writeUTF(dataName);
+      out.writeUTF("insert");
+      out.writeUTF(new Gson().toJson(email)); // json을 서버로 보내기
+      // 서버로부터 요청했던 데이터 읽어오기
+      return in.readUTF().equals("success");
+    }
   }
 
   public boolean update(Member email) throws Exception{
-    out.writeUTF(dataName);
-    out.writeUTF("update");
-    out.writeUTF(new Gson().toJson(email));
-    return in.readUTF().equals("success");
+    try(Socket socket =  new Socket(ip, port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())
+        ){
+      out.writeUTF(dataName);
+      out.writeUTF("update");
+      out.writeUTF(new Gson().toJson(email));
+      return in.readUTF().equals("success");
+    }
   }
 
   public Member findByEmail(String email)  throws Exception{
-    out.writeUTF(dataName);
-    out.writeUTF("findByEmail");
-    out.writeUTF(email);
-    if(in.readUTF().equals("fail")) {
-      return null;
+    try(Socket socket =  new Socket(ip, port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())
+        ){
+      out.writeUTF(dataName);
+      out.writeUTF("findByEmail");
+      out.writeUTF(email);
+      if(in.readUTF().equals("fail")) {
+        return null;
+      }
+      return  new Gson().fromJson( in.readUTF(), Member.class);
     }
-    return  new Gson().fromJson( in.readUTF(), Member.class);
   }
 
   public boolean delete(String email) throws Exception{
-    out.writeUTF(dataName);
-    out.writeUTF("delete");
-    out.writeUTF(email);
-    return in.readUTF().equals("success");
+    try(Socket socket =  new Socket(ip, port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())
+        ){
+      out.writeUTF(dataName);
+      out.writeUTF("delete");
+      out.writeUTF(email);
+      return in.readUTF().equals("success");
+    }
   }
 
   public Member[] findAll() throws Exception{
-    out.writeUTF(dataName);
-    out.writeUTF("findAll");
+    try(Socket socket =  new Socket(ip, port);
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        DataInputStream in = new DataInputStream(socket.getInputStream())
+        ){
+      out.writeUTF(dataName);
+      out.writeUTF("findAll");
 
-    if(in.readUTF().equals("fail")) {
-      return null;
+      if(in.readUTF().equals("fail")) {
+        return null;
+      }
+
+      return new Gson().fromJson( in.readUTF(), Member[].class);
     }
-
-    return new Gson().fromJson( in.readUTF(), Member[].class);
   }
 
 
