c6b1d71cbb78c296c2154fe8f2e282f26ee4bba8
Dingadung <1229jimin@naver.com>
2022-08-22 12:46:50 +0900

    Add tyy-catch server in RequestThread class

--- board-app/app-server/src/main/java/com/bitcamp/board/RequestThread.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/RequestThread.java
@@ -1,5 +1,7 @@
 package com.bitcamp.board;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Map;
 import com.bitcamp.servlet.Servlet;
@@ -14,4 +16,34 @@ public class RequestThread extends Thread{
   }
 
   // 별도의 실행흐름에서 수행할 작업 정의
+  @Override
+  public void run() {
+    try (Socket socket = this.socket;
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
+
+      System.out.println("클라이언트와 연결 되었음!");
+
+      // 클라이언트와 서버 사이에 정해진 규칙(protocol)에 따라 데이터를 주고 받는다.
+      String dataName = in.readUTF();
+
+      if (dataName.equals("exit")) {
+        break;
+      }
+
+      Servlet servlet = servletMap.get(dataName);
+      if (servlet != null) {
+        servlet.service(in, out);
+      } else {
+        out.writeUTF("fail");
+      }
+
+      System.out.println("클라이언트와 연결을 끊었음!");
+    } // 안쪽 try
+    catch(Exception e) {
+      System.out.println("클라이언트 여청 중 오류 발생!");
+      e.printStackTrace();
+    }
+    super.run();
+  }
 }
