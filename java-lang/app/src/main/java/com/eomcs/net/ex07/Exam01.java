4e86b3c4e01930b921bd6e794f7baff04af403ee
Dingadung <1229jimin@naver.com>
2022-08-24 19:58:55 +0900

    study-note java

--- java-lang/app/src/main/java/com/eomcs/net/ex07/Exam01.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex07/Exam01.java
@@ -6,24 +6,24 @@ import java.net.URL;
 public class Exam01 {
 
   public static void main(String[] args) throws Exception {
-    // 웹 상에서 자원의 위치를 표현하는 방법
-    // => [프로토콜]://서버주소:포트번호/자원의경로?파라미터명=값&파라미터명=값
-    // - 프로토콜: http(80), https(443), ftp(21/20) 등
-    // - 서버주소: IP 주소(192.168.0.1), 도메인명(www.bitcamp.co.kr)
-    // - 포트번호: 80 또는 443(생략할 수 있다), 8080(프록시 서버) 등
-    // - 자원의경로: /index.html, /board/list.jsp 등
-    // - 서버에 보내는 파라미터(Query String): 파라미터명=값&파라미터명=값
+    //     웹 상에서 자원의 위치를 표현하는 방법
+    //     => [프로토콜]://서버주소:포트번호/자원의경로?파라미터명=값&파라미터명=값
+    //     - 프로토콜: http(80), https(443), ftp(21/20) 등
+    //     - 서버주소: IP 주소(192.168.0.1), 도메인명(www.bitcamp.co.kr)
+    //     - 포트번호: 80 또는 443(생략할 수 있다), 8080(프록시 서버) 등
+    //     - 자원의경로: /index.html, /board/list.jsp 등
+    //     - 서버에 보내는 파라미터(Query String): 파라미터명=값&파라미터명=값
     //
     // 자원
-    // - 정적 자원(static)
-    //   - 요청할 때 마다 결과 콘텐트가 변경되지 않는 자원. 즉 파일을 가리킨다.
-    //   - 예) HTML, GIF, JPEG, PNG, CSS, JavaScript, TXT 등의 파일
-    // - 동적 자원(dynamic)
-    //   - 요청할 때 마다 결과 콘텐트가 변할 수 있는 자원. 
-    //   - 메일 조회, 게시물 변경, 주문 등의 웹 프로그램을 가리킨다.
-    //   - 예) index.php, index.jsp, /board/list 등
+    //     - 정적 자원(static)
+    //       - 요청할 때 마다 결과 콘텐트가 변경되지 않는 자원. 즉 파일을 가리킨다.
+    //       - 예) HTML, GIF, JPEG, PNG, CSS, JavaScript, TXT 등의 파일
+    //     - 동적 자원(dynamic)
+    //       - 요청할 때 마다 결과 콘텐트가 변할 수 있는 자원. 
+    //       - 메일 조회, 게시물 변경, 주문 등의 웹 프로그램을 가리킨다.
+    //       - 예) index.php, index.jsp, /board/list 등
     //
-    URL url = new URL("https://search.naver.com:443/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=bitcamp");
+    URL url = new URL("https://blog.naver.com/1229jimin");
 
     // URL 분석
     System.out.printf("프로토콜: %s\n", url.getProtocol());
--- java-lang/app/src/main/java/com/eomcs/net/ex07/Exam05.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex07/Exam05.java
@@ -11,7 +11,7 @@ public class Exam05 {
     // - 자원의 경로
     //      /드라이브명:/디렉토리 또는 파일 경로  (Windows)
     //      /루트디렉토리/디렉토리 또는 파일 경로 (Linux/macOS/Unix)
-    URL url = new URL("file:///c:/Users/user/git/bitcamp-study/Hello.java");
+    URL url = new URL("file:///Users/vivi/git/bitcamp-study/Hello.java");
 
     // URL 분석
     System.out.printf("프로토콜: %s\n", url.getProtocol());
