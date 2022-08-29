96044b4aa47c5f89386ea62ffa55567ad42ce43c
Dingadung <1229jimin@naver.com>
2022-08-29 14:13:17 +0900

    Add Test class to test Board2, constructor

--- board-app/app-common/src/main/java/com/bitcamp/board/domain/Test.java
+++ board-app/app-common/src/main/java/com/bitcamp/board/domain/Test.java
@@ -4,14 +4,11 @@ public class Test {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    Board2 b = new Board2();
+    Board2 b = new Board2("");
     b.writer = "홍";
-    b.no = 1;
     b.title = "okok";
     b.content = "nono";
     b.password = "1111";
-    b.viewCount = 1234;
-    b.createdDate = System.currentTimeMillis();
   }
 
 }
