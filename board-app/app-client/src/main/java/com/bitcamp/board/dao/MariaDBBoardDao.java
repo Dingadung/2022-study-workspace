9a5a17d0c8855b3c8cb65cdce432ad1c8fda7f33
Dingadung <1229jimin@naver.com>
2022-09-08 10:33:04 +0900

    Edit MemberDBMemberDao class connection 공유

--- board-app/app-client/src/main/java/com/bitcamp/board/dao/MariaDBBoardDao.java
+++ board-app/app-client/src/main/java/com/bitcamp/board/dao/MariaDBBoardDao.java
@@ -9,11 +9,14 @@ import java.util.List;
 import com.bitcamp.board.domain.Board;
 
 public class MariaDBBoardDao {
+  Connection con;
+  public MariaDBBoardDao() throws Exception {
+    con = DriverManager.getConnection(
+        "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
+  }
 
   public int insert(Board board)throws Exception {
     try(
-        Connection con = DriverManager.getConnection(
-            "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
         PreparedStatement pstmt = con.prepareStatement( 
             "insert into app_board(title, content, mno) values(?, ?, ?)");
         )
@@ -27,8 +30,6 @@ public class MariaDBBoardDao {
 
   public int update(Board board) throws Exception{
     try(
-        Connection con = DriverManager.getConnection( // 얘네가 네트워크 통신을 대신 처리해서 우리가 socket 처리를 일일히 할 필요가 없다.
-            "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
         PreparedStatement pstmt = con.prepareStatement(
             "update app_board set title=?, content = ? where bno = ?") // 값을 넣어야 할 자리를 ?로 표시한다. (in-parameter)
         ){
@@ -42,8 +43,6 @@ public class MariaDBBoardDao {
 
   public Board findByNo(int no)  throws Exception{
     try(
-        Connection con = DriverManager.getConnection(
-            "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
         PreparedStatement pstmt = con.prepareStatement(
             "select bno, title, content, mno, cdt, vw_cnt from app_board where bno =" + no); // 값을 넣어야 할 자리를 ?로 표시한다. (in-parameter)
         // no 자체가 int 타입이라 이런식으로 가능,String은 공격 위험 있어서 불가.
@@ -66,8 +65,6 @@ public class MariaDBBoardDao {
 
   public int delete(int no) throws Exception{
     try(
-        Connection con = DriverManager.getConnection( // 얘네가 네트워크 통신을 대신 처리해서 우리가 socket 처리를 일일히 할 필요가 없다.
-            "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
         PreparedStatement pstmt = con.prepareStatement( "delete from app_board where bno = ?")
         ) //try ()
     {
@@ -78,7 +75,6 @@ public class MariaDBBoardDao {
 
   public List<Board> findAll() throws Exception{
     try(
-        Connection con = DriverManager.getConnection( "jdbc:mariadb://localhost:3306/studydb", "study", "1111");
         PreparedStatement pstmt = con.prepareStatement( "select bno, title, mno, cdt, vw_cnt from app_board");
         ResultSet rs = pstmt.executeQuery()
         ) // try()
