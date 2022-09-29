b2d0c5678c03820490c16d0dbcbacbe6b14d5a47
Dingadung <1229jimin@naver.com>
2022-09-29 10:50:25 +0900

    Add AdminCheckFilter class

--- board-app/app-server/src/main/java/com/bitcamp/board/filter/AdminCheckFilter.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/filter/AdminCheckFilter.java
@@ -11,29 +11,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.bitcamp.board.domain.Member;
 
-@WebFilter("*")
-public class LoginCheckFilter implements Filter {
+@WebFilter("/member/*")
+public class AdminCheckFilter implements Filter {
 
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
-    System.out.println("LoginCheckFilter 실행!");
-    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    System.out.println("AdminCheckFilter 실행!");
 
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
     HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-    String servletPath = httpRequest.getServletPath();
-
-    if(servletPath.endsWith("add") || servletPath.endsWith("update") ||  servletPath.endsWith("delete") ) {
-      Member loginMember =(Member)httpRequest.getSession().getAttribute("loginMember");
-      if(loginMember == null) { // 로그인 하지 않았다면
-        httpResponse.sendRedirect(httpRequest.getContextPath() + "/auth/form.jsp");
-        return;
-      }
+    Member loginMember =(Member)httpRequest.getSession().getAttribute("loginMember");
+    if(!loginMember.getEmail().equals("admin@test.com")) { // 관리자가 아니라면
+      httpResponse.sendRedirect(httpRequest.getContextPath() + "/");
+      return;
     }
 
-
     chain.doFilter(request, response);
-  }
-
-}
+  } // doFilter()
+} // Filter{}
