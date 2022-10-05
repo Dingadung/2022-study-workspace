2247a0dc9fae52034dc38c16487d203a0f7c3d01
Dingadung <1229jimin@naver.com>
2022-10-05 11:10:19 +0900

    js-study-ch1,2-jimin

--- board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardDeleteController.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardDeleteController.java
@@ -1,46 +1,52 @@
 package com.bitcamp.board.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.bitcamp.board.dao.BoardDao;
 import com.bitcamp.board.domain.Member;
 
 @WebServlet("/board/delete")
 public class BoardDeleteController extends HttpServlet{
 
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  BoardDao boardDao;
+    BoardDao boardDao;
 
-  @Override
-  public void init() {
-    boardDao = (BoardDao) this.getServletContext().getAttribute("boardDao");
-  }
-
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    try {
-      int no = Integer.parseInt(request.getParameter("no"));
-
-      Member loginMember = (Member) request.getSession().getAttribute("loginMember");
-      if (boardDao.findByNo(no).getWriter().getNo() != loginMember.getNo()) {
-        throw new Exception("게시글 작성자가 아닙니다.");
-      }
-
-      if(boardDao.delete(no) == 0) {
-        throw new Exception("게시글 등록 실패"); 
-      }
-
-      response.sendRedirect("list");
-
-    } catch(Exception e) {
-      request.setAttribute("exception", e);
-      request.getRequestDispatcher("/error.jsp").forward(request, response);
+    @Override
+    public void init() {
+        boardDao = (BoardDao) this.getServletContext().getAttribute("boardDao");
     }
-  }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            int no = Integer.parseInt(request.getParameter("no"));
+
+            Member loginMember = (Member) request.getSession().getAttribute("loginMember");
+            if (boardDao.findByNo(no).getWriter().getNo() != loginMember.getNo()) {
+                throw new Exception("게시글 작성자가 아닙니다.");
+            }
+
+            // 첨부파일 삭제
+            boardDao.deleteFiles(no);
+
+            // 게시글 삭제
+            if(boardDao.delete(no) == 0) {
+                throw new Exception("게시글 등록 실패"); 
+            }
+
+            response.sendRedirect("list");
+
+        } catch(Exception e) {
+            request.setAttribute("exception", e);
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+    }
 }
