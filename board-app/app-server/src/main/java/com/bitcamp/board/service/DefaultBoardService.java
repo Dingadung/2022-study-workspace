ee3630d658ca99313277d557f51aec93e25ff118
Dingadung <1229jimin@naver.com>
2022-10-06 11:15:47 +0900

    074. 트랜잭션 적용하기 2단계 - 게시글 입력과 변경, 삭제에 트랜잭션을 적용한다..

--- board-app/app-server/src/main/java/com/bitcamp/board/service/DefaultBoardService.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/service/DefaultBoardService.java
@@ -34,38 +34,57 @@ public class DefaultBoardService implements BoardService{
         } catch (Exception e) {
             con.rollback();
             throw e;
+
+        } finally {
+            con.setAutoCommit(true);
         }
     }
 
     @Override
     public boolean update(Board board) throws Exception {
-        // 1) 게시글 변경
-        if (boardDao.update(board) == 0) {
-            return false;
-        }
-        // 2) 첨부파일 추가
-        boardDao.insertFiles(board);
+        con.setAutoCommit(false);
+        try {        
+            // 1) 게시글 변경
+            if (boardDao.update(board) == 0) {
+                return false;
+            }
+            // 2) 첨부파일 추가
+            boardDao.insertFiles(board);
 
-        return true;
+            con.commit();
+            return true;
+        } catch (Exception e) {
+            con.rollback();
+            throw e;
+        } finally {
+            con.setAutoCommit(true);
+        }
     }
 
     @Override
     public Board get(int no) throws Exception {
-        // 이 메서드의 경우 하는 일이 없다.
-        // 그럼에도 불구하고 이렇게 하는 이유는 일관성을 위해서다.
-        // 즉 Controller는 Service 객체를 사용하고 Service 객체는 DAO를 사용하는 형식을 
-        // 지키기 위함이다.
-        // 사용 규칙이 동일하면 프로그래밍을 이해하기 쉬워진다.
         return boardDao.findByNo(no);
     }
 
     @Override
     public boolean delete(int no) throws Exception {
-        // 1) 첨부파일 삭제
-        boardDao.deleteFiles(no);
+        con.setAutoCommit(false);
+        try {   
+            // 1) 첨부파일 삭제
+            boardDao.deleteFiles(no);
 
-        // 2) 게시글 삭제
-        return boardDao.delete(no) > 0;
+            // 2) 게시글 삭제
+            boolean result = boardDao.delete(no) > 0;
+
+            con.commit();
+
+            return result;
+        } catch (Exception e) {
+            con.rollback();
+            throw e;
+        } finally {
+            con.setAutoCommit(true);
+        }
     }
 
     @Override
