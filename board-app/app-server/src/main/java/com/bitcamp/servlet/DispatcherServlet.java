0295714741ccde26fb4548270a9a4a6cef2cd4a8
Dingadung <1229jimin@naver.com>
2022-10-07 11:31:01 +0900

    077. Front Controller 도입 1단계 - 프론트 컨트롤러 역할을 수행할 클래스를 만든다. && Add - js-study-ch6-jimin

---
+++ board-app/app-server/src/main/java/com/bitcamp/servlet/DispatcherServlet.java
@@ -0,0 +1,31 @@
+package com.bitcamp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 역할:
+// - 페이지 컨트롤러의 앞쪽에서 클라이언트 요청을 받는 일을 한다.
+// - 클라이언트가 요청한 경로에 따라 적절한 페이지 컨트롤러를 실행한다.
+// - 페이지 컨트롤러의 공통 기능을 수행한다.
+//
+//@WebServlet(value = "/service/*")
+public class DispatcherServlet extends HttpServlet{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 프론트 컨트롤러를 경유해서 실행할 페이지 컨트롤러의 경로를 알아낸다.
+        // "/service" 다음에 오는 경로, 즉 *에 해당하는 경로를 리턴한다.
+        String pathInfo = req.getPathInfo();
+
+        // 페이지 컨트롤러로 실행을 위임한다.
+        resp.setContentType("text/html;charset=UTF-8");
+        RequestDispatcher 요청배달자 = req.getRequestDispatcher(pathInfo);
+        요청배달자.include(req, resp);
+    }
+}
